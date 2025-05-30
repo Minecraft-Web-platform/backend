16d29c158609741583b81478ccf974debbb5d94f
oleksandrshtonda <oleksandrshtonda@gmail.com>
2025-05-30 22:19:14 +0200

    setup esling and prettier

---
+++ .eslintrc.js
@@ -0,0 +1,38 @@
+module.exports = {
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+      project: 'tsconfig.json',
+      sourceType: 'module',
+    },
+    plugins: ['@typescript-eslint', 'prettier'],
+    extends: [
+      'plugin:@typescript-eslint/recommended',
+      'plugin:prettier/recommended',
+    ],
+    root: true,
+    env: {
+      node: true,
+      jest: true,
+    },
+    rules: {
+      'prettier/prettier': ['error'],
+
+      '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+      '@typescript-eslint/explicit-function-return-type': 'off',
+      '@typescript-eslint/no-explicit-any': 'warn',
+      '@typescript-eslint/consistent-type-imports': 'error',
+      'import/order': [
+        'error',
+        {
+          groups: [['builtin', 'external'], 'internal', ['parent', 'sibling', 'index']],
+          'newlines-between': 'always',
+          alphabetize: { order: 'asc', caseInsensitive: true },
+        },
+      ],
+
+    
+      'no-console': 'warn',
+      'no-debugger': 'warn',
+    },
+  };
+  
