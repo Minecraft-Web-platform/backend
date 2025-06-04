dddb42cd7c09c8456dde5472289f67df26f3a0d9
oleksandrshtonda <oleksandrshtonda@gmail.com>
2025-06-04 11:22:01 +0200

    created dto

---
+++ src/auth/dtos/create-user.dto.ts
@@ -0,0 +1,10 @@
+import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
+
+export class CreateUserDTO {
+  @IsString()
+  @IsNotEmpty()
+  username: string;
+
+  @IsEmail()
+  email: string;
+}
