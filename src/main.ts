0472f9cbb900daf80abd9c32ed093900e49460ed
oleksandrshtonda <oleksandrshtonda@gmail.com>
2025-09-13 17:14:41 +0200

    added cors

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,16 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { logger: ['log', 'error', 'fatal', 'warn'] });
 
+  app.enableCors({
+    origin: [
+      'https://khroniki-kraya.com',
+      'https://www.khroniki-kraya.com',
+      'https://frontend-bay-eight-67.vercel.app',
+    ],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
+
   const swaggerConfig = new DocumentBuilder()
     .setTitle('Minecraft website API')
     .setDescription('The APIs of all services')
