e706064fb0067fbeb0bc0920b0eb49b7736489b0
oleksandrshtonda <oleksandrshtonda@gmail.com>
2025-09-26 02:50:54 +0200

    fixed

--- src/email/email.service.ts
+++ src/email/email.service.ts
@@ -1,42 +1,29 @@
 import { Injectable, UnprocessableEntityException } from '@nestjs/common';
-import { Transporter, createTransport } from 'nodemailer';
 
 import { EmailServiceContract } from './email.service.contract';
 import { MailTemplateStrategy } from './strategies/mail-template.strategy';
 import { ConfigService } from '@nestjs/config';
+import { Resend } from 'resend';
 
 @Injectable()
 export class EmailService implements EmailServiceContract {
-  private readonly transporter: Transporter;
+  private readonly resend: Resend;
 
   constructor(private readonly configService: ConfigService) {
-    this.configService = configService;
-    const options = {
-      host: this.configService.get<string>('SMTP_HOST'),
-      port: this.configService.get<number>('SMTP_PORT'),
-      secure: this.configService.get<number>('SMTP_PORT') == 465,
-      auth: {
-        user: this.configService.get<string>('SMTP_USER'),
-        pass: this.configService.get<string>('SMTP_PASS'),
-      },
-    };
-
-    this.transporter = createTransport(options, { debug: true });
+    this.resend = new Resend(this.configService.get<string>('RESEND_API_KEY'));
   }
 
   async send(to: string, mailTemplate: MailTemplateStrategy): Promise<void> {
-    console.log('start sending');
-
     try {
-      await this.transporter.sendMail({
-        to: to,
+      await this.resend.emails.send({
+        from: this.configService.get<string>('SMTP_FROM') ?? 'send@khroniki-kraya.com',
+        to: 'oleksandr.shtonda.dev@gmail.com',
         subject: mailTemplate.getSubject(),
         html: mailTemplate.getHTML(),
         text: mailTemplate.getText(),
       });
-
-      console.log('stop sending');
-    } catch {
+    } catch (error) {
+      console.error('Resend error:', error);
       throw new UnprocessableEntityException(`Email "${to}" is invalid or cannot be delivered`);
     }
   }
