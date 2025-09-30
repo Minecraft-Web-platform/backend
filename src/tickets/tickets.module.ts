eb6c3ffd99a7c36e427919d1c884e14089a4c047
oleksandrshtonda <oleksandrshtonda@gmail.com>
2025-10-01 00:52:38 +0200

    added email require

--- src/tickets/tickets.module.ts
+++ src/tickets/tickets.module.ts
@@ -3,9 +3,10 @@ import { EmailModule } from 'src/email/email.module';
 import { TicketsService } from './tickets.service';
 import { EmailService } from 'src/email/email.service';
 import { TicketsController } from './tickets.controller';
+import { UsersModule } from 'src/users/users.module';
 
 @Module({
-  imports: [EmailModule],
+  imports: [EmailModule, UsersModule],
   providers: [TicketsService, EmailService],
   controllers: [TicketsController],
 })
--- src/tickets/tickets.service.ts
+++ src/tickets/tickets.service.ts
@@ -1,14 +1,28 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
 import { EmailService } from 'src/email/email.service';
 import { TicketStrategy } from 'src/email/strategies/ticket.strategy';
 import { TicketDTO } from './dtos/ticket.dto';
+import { UsersService } from 'src/users/users.service';
 
 @Injectable()
 export class TicketsService {
-  constructor(private readonly emailService: EmailService) {}
+  constructor(
+    private readonly emailService: EmailService,
+    private readonly userService: UsersService,
+  ) {}
 
   public async sendTicket(body: TicketDTO) {
     const { username, email, topic, content } = body;
+    const userInDB = await this.userService.getByUsername(username.toLowerCase());
+
+    if (!userInDB?.email) {
+      throw new ForbiddenException('Добавь почту!');
+    }
+
+    if (!userInDB?.emailIsConfirmed) {
+      throw new ForbiddenException('Подтверди почту');
+    }
+
     const strategy = new TicketStrategy(username, topic, content, email);
 
     return this.emailService.send('oleksandr.shtonda.dev@gmail.com', strategy);
