eea8e660e5f5c8bd2ae85d7b10e29914ca17ceba
oleksandrshtonda <oleksandrshtonda@gmail.com>
2025-10-15 18:07:17 +0200

    setup module

--- src/news/news.module.ts
+++ src/news/news.module.ts
@@ -7,16 +7,16 @@ import { NewsController } from './news.controller';
 import { News } from './entities/news.entity';
 import { NewsBlock } from './entities/news-block.entity';
 import { NewsCategory } from './entities/news-category.entity';
-import { AuthModule } from 'src/auth/auth.module';
 import { OwnJwtModule } from 'src/own-jwt/own-jwt.module';
 import { UsersModule } from 'src/users/users.module';
 import { NewsCategoryService } from './news-category.service';
 import { NewsCategoryController } from './news-category.controller';
+import { NewsBlockService } from './news-blocks.service';
+import { NewsAdminController } from './news-admin.controller';
 
 @Module({
   imports: [TypeOrmModule.forFeature([News, NewsBlock, NewsCategory]), OwnJwtModule, UsersModule],
-  providers: [NewsService, NewsCategoryService],
-  controllers: [NewsController, NewsCategoryController],
-  exports: [NewsService],
+  providers: [NewsService, NewsCategoryService, NewsBlockService],
+  controllers: [NewsController, NewsCategoryController, NewsAdminController],
 })
 export class NewsModule {}
